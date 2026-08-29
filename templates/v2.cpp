f127fa3c87ccc7541ee3af363565ee7268f69d0c
Md. Naimur Rahman <mdnaimur020@gmail.com>
2026-08-29 11:56:38 +0600

    feat: improve the template and create v2.cpp

---
+++ templates/v2.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem: $name$
+ * URL: $url$
+ * Date: $date$
+ * Time: $time$
+ */
+
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+
+void solve() {
+  //$CURSOR_PLACEHOLDER
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+  while (t--) solve();
+
+  return 0;
+}
